d45c0106f20a85d9a00a090e80c5830c7fd89377
Nicole Ohel <nicole.ohel@gmail.com>
2017-12-04 14:44:15 -0800

    Made a registration form that shows some info about the user once it is submitted. Created a user class and and address class to make organizing/binding to the information from the wform easier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {User} from './user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  user = new User();
+  submitted:boolean;
+  states = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WI','WY']
+  confirm = '';
+  subUser:User;
+
+  onSubmit() {
+    this.subUser = this.user
+    this.user = new User();
+    this.submitted = true
+  }
+  ngOnInit() {
+    this.submitted = false;
+    this.subUser = undefined;
+  }
 }
